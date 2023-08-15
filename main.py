13a3c66852f0874d6ec7726b6b20abb8c3d8086c
TheUserOne <theuserone@yandex.ru>
2023-08-15 20:55:15 +0700

    beginning

---
+++ main.py
@@ -0,0 +1,59 @@
+# n = float(input())
+# if n == 666:
+#     print('Welcome back Mr.Devil')
+# elif n == 13:
+#     print('You are 13th')
+# else:
+#     print(f"your number is {n}")
+
+
+
+
+# n = int(input())
+# flag = True
+# i = 2
+# while flag:
+#     if n % i == 0:
+#         flag = False
+#         print(i)
+#     elif i > n // 2:
+#         print (n)
+#         flag = False
+#     i += 1
+
+
+
+
+# r = range(100, 0 , -20)
+
+# for i in r:
+#     print(i)
+
+
+
+
+# a = 'qwerty'
+# for i in a:
+#     print(i)
+
+
+
+
+
+# line = ""
+# for i in range(5):
+#     line = ""
+#     for j in range(5):
+#         line += "*"
+#     print(line)
+
+
+
+# text = 'sample pample'
+# print(text.replace('sample', 'Sample'))
+
+
+
+text = 'There was a hole. It\'s gone now'
+
+print(text[0:3] + text[7] + text[-3:])
